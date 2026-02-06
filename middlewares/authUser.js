014a682b78efd6196b6fc3ce47bd881d21bfc8da
anantharkrishnan <ananthakrishnan1102@gmail.com>
2026-02-06 05:59:50 +0530

    fix auth middleware

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -1,35 +1,24 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const authUser=(req,res,next)=>{
-    try {
-         
+const authUser = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
 
-        const {token}=req.cookies  
-           
-            
-         if(!token){
-           return res.status(401).json({error:"unauthorized user"})
-
-        }
-       
-        
-         const decodedToken= jwt.verify(token,process.env.JWT_SECRET_KEY)
-         
-         if(!decodedToken){
-             return res.status(401).json({error:"unauthorized user"})
-             
-         }
-          req.user={ id: decodedToken.id, role: decodedToken.role };
-            
-       
-
-           next()
-        
-        
-    } catch (error) {
-        console.log(error);
-        
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "unauthorized user" });
     }
-}
 
-module.exports= authUser
+    const token = authHeader.split(" ")[1];
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = { id: decodedToken.id, role: decodedToken.role };
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ error: "invalid token" });
+  }
+};
+
+module.exports = authUser;
